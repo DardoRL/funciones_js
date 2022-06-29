9fa1a1b4352560d4dbea7542c71950b487e26f9b
DardoRL <69401941+DardoRL@users.noreply.github.com>
2022-06-29 14:25:26 -0300

    Add files via upload

--- ejercicios_profundizacion/js/profundizacion_1.js
+++ ejercicios_profundizacion/js/profundizacion_1.js
@@ -12,3 +12,15 @@
  *    Debe recibir 5 números como argumento y calcular el promedio.  Debe reportar por
  *    medio de alert(); y console.log(); el detalle de los datos recibidos.
 */
+"use strict";
+function promedio(numero1, numero2, numero3, numero4, numero5) {
+    console.log("Funcion promedio");
+    let promedio = (numero1 + numero2 + numero3 + numero4 + numero5) / 5;
+    console.log (`Los numeros ingresados fueron: ${numero1}, ${numero2} , ${numero3} , ${numero4} y ${numero5}`);
+    alert(`Los numeros ingresados fueron: ${numero1}, ${numero2}, ${numero3}, ${numero4} y ${numero5} `)
+    console.log ("El promedio de los 5 numeros ingresados es: " + promedio);
+    alert(`El promedio de los 5 numeros ingresados es: ${promedio}`)
+    
+}
+
+promedio(10, 5, 3, 8, 21);
--- ejercicios_profundizacion/js/profundizacion_2.js
+++ ejercicios_profundizacion/js/profundizacion_2.js
@@ -13,3 +13,33 @@
  *    Según la cantidad especificada utilice condicionales para solicitar la cantidad de nombres
  *    correctos.
 */
+"use strict";
+
+let cantInvitados;
+let invitados;
+let invitado1;
+let invitado2;
+let invitado3
+function generarInvitados(){
+    cantInvitados = prompt('Ingrese la cantidad de invitados:');
+    if (cantInvitados==1){
+        invitado1 = prompt('Ingrese el nombre del invitado:');
+        invitados = `${invitado1}`;
+    }
+    else if (cantInvitados==2){
+        invitado1 = prompt('Ingrese el nombre del primer invitado:');
+        invitado2 = prompt('Ingrese el nombre del segundo invitado:');
+        invitados = `${invitado1} y ${invitado2}`;
+    }
+    else if (cantInvitados==3) {
+        invitado1 = prompt('Ingrese el nombre del primer invitado:');
+        invitado2 = prompt('Ingrese el nombre del segundo invitado:');
+        invitado3 = prompt('Ingrese el nombre del tercer invitado:');
+        invitados = `${invitado1}, ${invitado2} y ${invitado3}`;
+    }  
+    return invitados;
+    }
+
+// Definir y crear la función antes de esta llamada
+invitados = generarInvitados();
+console.log(`Mis invitados: ${invitados}`)
